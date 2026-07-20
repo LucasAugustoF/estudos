6d526ba3cbef1aaa09dceac119d8a0d4449cae71
Lucas Augusto <lucas.ferreira.git@gmail.com>
2026-07-20 17:51:04 -0300

    Adicionando a aula 4

---
+++ 04 - 10. Comentários de código/index.js
@@ -0,0 +1,12 @@
+//escrever um comentário
+console.log('Olá, mundo!');
+console.log('Linha 1');
+console.log('Linha 2'); //Aqui tem outro comentário
+console.log('Linha 3');
+//Outro comentário
+console.log('Linha 4');
+//Outro comentário
+
+/* 
+Comentário de bloco
+*/
