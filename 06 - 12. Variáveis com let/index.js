48263848be7e9114e0c9b15e3b7ddf2d5eccd53a
Lucas Augusto <lucas.ferreira.git@gmail.com>
2026-07-21 23:58:10 -0300

    adicionado aula 06 de js e typescript

---
+++ 06 - 12. Variáveis com let/index.js
@@ -0,0 +1,12 @@
+let nome; //Declarou a variável
+nome = 'Qualquer valor'; //Iniciando a variável
+console.log(nome);
+
+nome = 'Otavio'; //Reatribuindo a variável
+console.log(nome);
+
+
+//não podecriar variaveis com palavras reservadas
+//let if
+//let var
+//não pode começar o nome de uma variável com um número
