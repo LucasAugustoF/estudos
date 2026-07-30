32afcb7fe4ef728eca891346a4f1a0dca235af4d
Lucas Augusto <lucas.ferreira.git@gmail.com>
2026-07-29 21:51:12 -0300

    aula e exercicio

---
+++ 12 - 21. alert, confirm e prompt (Navegador)/teste.js
@@ -0,0 +1,5 @@
+const num1 = prompt("Digite o primeiro número:");
+const num2 = prompt("Digite o segundo número:");
+
+const resultado = parseFloat(num1) + parseFloat(num2);
+alert(`O resultado da soma é: ${resultado}`);
