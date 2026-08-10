4425bb205b0298531a1166287b30d4c442ca3d10
Lucas Augusto <lucas.ferreira.git@gmail.com>
2026-08-09 21:47:44 -0300

    att nova aula

---
+++ 14 - 24. Mais sobre strings/index.js
@@ -0,0 +1,6 @@
+let umaString = "Um \"texto\""; // O caractere de escape \" permite que você use aspas dentro da string sem encerrar a string.
+let outraString = 'Um texto'; 
+let terceiraString = "Um texto";
+console.log(umaString);
+console.log(outraString[4]); // Acessando o caractere na posição 4 da string "outraString", que é a letra "t".
+console.log(terceiraString.charAt(5)); // Usando o método charAt() para acessar o caractere na posição 6 da string "terceiraString", que é a letra "o".
