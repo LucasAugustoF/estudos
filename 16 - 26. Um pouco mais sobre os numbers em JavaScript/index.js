0eed36fdd6366e8a78a28a5442ca3567a452d2f0
Lucas Augusto <lucas.ferreira.git@gmail.com>
2026-09-01 21:49:46 -0300

    att

--- 16 - 26. Um pouco mais sobre os numbers em JavaScript/index.js
+++ 16 - 26. Um pouco mais sobre os numbers em JavaScript/index.js
@@ -1,7 +1,13 @@
-let num1 =1;
+let num1 =10;
 let num2 =2.5;
+let num3 = 10.571283823;
+let numBinaria = 1500;
 
 console.log(num1 + num2);
 console.log(num1.toString() + num2); // Concatenação transformando num1 em string resultando em "12.5"
 console.log(typeof num1); // Continua sendo um number
+console.log(numBinaria.toString(2)); // Transformando o número em binário
+console.log(num3.toFixed(2)); // Limitando o número de casas decimais para 2 
+
+console.log(Number.isInteger(num1)); // Verificando se o número é inteiro
 
