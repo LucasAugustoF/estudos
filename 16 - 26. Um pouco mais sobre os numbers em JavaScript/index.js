4da578adae78df6ea926671fbf5a72eff17c29f2
Lucas Augusto <lucas.ferreira.git@gmail.com>
2026-09-01 21:20:31 -0300

    atualização

---
+++ 16 - 26. Um pouco mais sobre os numbers em JavaScript/index.js
@@ -0,0 +1,7 @@
+let num1 =1;
+let num2 =2.5;
+
+console.log(num1 + num2);
+console.log(num1.toString() + num2); // Concatenação transformando num1 em string resultando em "12.5"
+console.log(typeof num1); // Continua sendo um number
+
